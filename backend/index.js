02d26bf43a66202ba9484fcd40c3a0d1757d911d
Jerry Luo <jerryluoaustin@gmail.com>
2023-09-17 22:08:55 -0500

    First commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 import express from "express"
 import cors from "cors"
+import axios from "axios"
+import env from 'dotenv'
 
 const app = express()
+env.config();
+
+const API_KEY = process.env.API_KEY;
+const options = {
+  method: 'GET',
+  url: 'https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/',
+  headers: {
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': 'coronavirus-smartable.p.rapidapi.com'
+  }
+};
+
+
+const response = await axios.request(options);
+const totalConfirmedCases = response.data.stats.totalConfirmedCases
+const newlyConfirmedCases = response.data.stats.newlyConfirmedCases
+const totalDeaths = response.data.stats.totalDeaths
 
 app.use(cors({
     origin: "http://localhost:5500"
@@ -9,8 +28,9 @@ app.use(cors({
 
 app.get("/", (req, res) => {
     res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
+        x: ["Total Confirmed Cases", "Total Deaths", "Newly Confirmed Cases"],
+        y: [totalConfirmedCases, totalDeaths, newlyConfirmedCases],
+        type: 'bar'
     }).status(200)
 })
 
